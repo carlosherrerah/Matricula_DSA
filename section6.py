9abd331436dfe009be2187c3a723cea2bfb82281
carlosherrerah <carlos.herrera@upa.edu.mx>
2023-01-10 19:55:32 -0600

    Borrar section

--- section6.py
+++
@@ -1,8 +0,0 @@
-# section 5 comments
-# section 6 Interaction with the user
-print("What is your last name: ")
-lastName = input()
-Name = input("What is your name: ")
-Edad = float(input("How old are you: "))   # casting:   int() or float   
-print("Hi", Name, lastName, "you are ", Edad, "years old")
-
