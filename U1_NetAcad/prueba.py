416a96335f4bbd1026c9d03c1b7b66601bd2d8d6
carlosherrerah <carlos.herrera@upa.edu.mx>
2023-01-19 21:55:00 -0600

    230119

--- U1_NetAcad/prueba.py
+++ U1_NetAcad/prueba.py
@@ -4,4 +4,14 @@
 03Tickets.py
 '''
 
- 
+lista = [4,7,1]
+y=list(map(lambda x:x+3,lista))
+print(y)
+
+mayores = list(filter(lambda x: x>2, lista))
+print(mayores)
+
+ordenada = sorted(lista)
+print(ordenada)
+
+
