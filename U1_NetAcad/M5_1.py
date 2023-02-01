68329818341d133f1ee535567447ff6e402d8774
carlosherrerah <carlos.herrera@upa.edu.mx>
2023-02-01 15:57:09 -0600

    230102

--- U1_NetAcad/M5_1.py
+++ U1_NetAcad/M5_1.py
@@ -13,13 +13,15 @@
 # import random
 from random import randrange
 import os
+import math
 
 os.system("cls")
 
 # print(randrange(1,10))
-
+col = 3
+a=[]
 board = [[1,2,3],
-         [4,5,6],
+         [4,'X',6],
          [7,8,9]]
 
 def display_board(board):
@@ -27,13 +29,53 @@ def display_board(board):
     for i in range(3):
         for j in range(3):
             if j == 1:
-                #print("|   X   |   X   |   X   |")
-                print("|  ", board[i][j]," ","|  ", board[i][j]," ","|  ", board[i][j]," ","|")
+                print("|  ", board[i][j-1], " ",
+                      "|  ", board[i][j], " ",
+                      "|  ", board[i][j+1], " ", "|")
             else:
                 print("|       |       |       |")
         print("+-------+-------+-------+")
 display_board(board)
 
+def Fil(i):
+    f = math.floor( i / col)
+    f = f - 1 if i % int(col) == 0 else f
+    return f
+
+def Col(i):
+    c = i - Fil(i) * col - 1
+    return c
+
+'''
+int Fil(int i) {   // 7 = [2][0]
+    double col = 3.0;
+    int f;
+    f = floor(i / col);
+    f = (i % int(col) == 0) ? f - 1 : f;
+    return f;
+}
+
+int Col(int i) {
+    double col = 3.0;
+    int c = i - Fil(i) * col - 1;
+    return c;
+}
+'''
+
+def rand5():
+    i = 1
+    while i <= 5:
+        n = randrange(10)
+        if n not in a:
+            a.append(n)
+            i = i + 1
+
+
+
+rand5()
+print(a)
+print(Fil(7))
+print(Col(7))
 
 
 
