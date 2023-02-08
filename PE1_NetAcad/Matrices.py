0f89e8e2d12f377e6924a3e0b8eee6194fa4285e
carlosherrerah <carlos.herrera@upa.edu.mx>
2023-02-08 15:36:00 -0600

    230207

--- PE1_NetAcad/Matrices.py
+++ PE1_NetAcad/Matrices.py
@@ -1,3 +1,4 @@
+'''
 def multiply_matrix(A,B):
   global C
   if  A.shape[1] == B.shape[0]:
@@ -12,5 +13,17 @@ def multiply_matrix(A,B):
 
 [[sum(a*b for a, b in zip(A_row, B_col)) for B_col in zip(*B)]
     for A_row in A]
+'''
+# Matriz transpuesta
+def imprimirMatriz(M):
+  f = M.shape[0]
+  c = M.shape[1]
+  print(f,c)
 
 
+
+F = 4 
+C = 3
+A = [[C * i + j + 1 for j in range(C)] for i in range(F)]  # 4 X 3
+print(A)
+imprimirMatriz(A)
