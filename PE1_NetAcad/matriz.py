0690e021ccc370473fc9bde466322939f4e131dd
carlosherrerah <carlos.herrera@upa.edu.mx>
2023-02-10 15:55:48 -0600

    230209

--- PE1_NetAcad/matriz.py
+++ PE1_NetAcad/matriz.py
@@ -1,13 +1,15 @@
 import numpy as np
+import ctypes
 
 def multiply_matrix(A,B):
   # global C
   if  A.shape[1] == B.shape[0]:
     C = np.zeros((A.shape[0],B.shape[1]),dtype = int)
-    for row in range(A.shape[0]): 
-        for col in range(B.shape[1]):
-            for elt in range(len(B)):
-              C[row, col] += A[row, elt] * B[elt, col]
+    for i in range(A.shape[0]):       # M 
+        for j in range(B.shape[1]):   # N
+            for k in range(len(B)):   # P
+              # C[i, j] += A[i, k] * B[k, j]  # Funciona
+              C[i][j] += A[i][k] * B[k][j]    # Funciona 
     return C
   else:
     return "Sorry, cannot multiply A and B."
@@ -49,6 +51,15 @@ print(np.matmul(A,B))
 
 C = multiply_matrix(A,B)
 print(C)
+print(len(C))     # No. de Renglones
+print(np.size(C)) # No. de elementos
+print("Adress of C:", id(C))
+print("Adress of C:", id(C)+1)
+print(C[0][0], C[0,0])
+
+val_mi_id = ctypes.cast(id(C), ctypes.py_object).value
+print(val_mi_id[0][1])
+print(val_mi_id)
 
 
 
