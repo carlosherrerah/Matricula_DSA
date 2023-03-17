606bc04b9cd9ffcc3e832dc41204d0ebf15282af
carlosherrerah <carlos.herrera@upa.edu.mx>
2023-03-17 15:09:29 -0600

    230316

--- U3/colas.py
+++ U3/colas.py
@@ -29,21 +29,38 @@ class Queue:
             self.head = newNode
             self.tail = newNode
         else:
+            self.tail.next = newNode
+            self.tail = newNode
 
-            pass
-
+        
+    def dequeue(self):
+        data = None
+        if not self.isEmpty():
+            self.size-=1
+            old  = self.head
+            data = self.head.data
+            self.head = self.head.next
+            del old
+            if self.isEmpty():
+                self.tail = None
+        return data
 
 q = Queue()
 print(q.getSize())
 
 q.enqueue(5)
 q.enqueue(10)
+q.enqueue(15)
 
-# q.enqueue("Jesus")
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
 
 
-
-
-
-
-
+'''
+print(q.head.data)
+print(q.head.next.data)
+print(q.head.next.next.data)
+print(q.head.next.next.next.data)
+'''
