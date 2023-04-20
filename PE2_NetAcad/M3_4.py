2620c68ba7802a20defe48694e350e30b66507a9
carlosherrerah <carlos.herrera@upa.edu.mx>
2023-04-19 22:00:57 -0600

    anoche

--- PE2_NetAcad/M3_4.py
+++ PE2_NetAcad/M3_4.py
@@ -1,12 +1,17 @@
+'''
 # Seccion 4. POO: metodos
 # un método es una función que está dentro de una clase.
-# un método está obligado a tener al menos un parámetro
+# un método está obligado a tener al menos un parámetro.
 # un método puede invocarse sin un argumento, pero no puede declararse sin parámetros.
 # El primer (o único) parámetro generalmente se denomina self
 # self: identifica el objeto para el cual se invoca el método.
 # El parámetro self es usado para obtener acceso a la instancia del objeto y las variables de clase.
 # El parámetro self también se usa para invocar otros métodos desde dentro de la clase.
 # un método cuyo nombre comienza con __ está (parcialmente) oculto.
+
+# Si una clase contiene un constructor (un método llamado __init__), 
+#    este no puede devolver ningún valor y no se puede invocar directamente.
+
 
 class Classy:
     varia = 1
@@ -25,6 +30,7 @@ class Classy:
         print("oculto")
 
 # __bases__ es una tupla. La tupla contiene clases. solo las clases tienen este atributo, los objetos no.
+# __bases__ es una tupla que contiene las superclases de una clase.
 def printBases(cls):    # Indica la clase padre
     print('( ', end='')
     for x in cls.__bases__:
@@ -46,11 +52,11 @@ obj._Classy__hidden()
 print(obj.__dict__)
 print(Classy.__dict__)
 
-# Nombre de la clase
+# Nombre de la clase o donde esta viviendo
 print(Classy.__name__)
 print(type(obj).__name__)
 
-#  almacena el nombre del módulo que contiene la definición de la clase
+# almacena el nombre del módulo que contiene la definición de la clase
 # cualquier módulo llamado __main__  es el archivo actualmente en ejecución.
 print(Classy.__module__)
 print(obj.__module__)
@@ -89,5 +95,21 @@ print(obj.__dict__)
 incIntsI(obj)
 print(obj.__dict__)
 
-# resumen de seccion
-#     
+'''
+class Snake:
+    def __init__(self):
+        self.victims = 0
+
+    def __init__(self, value):
+        self.victims = value
+
+    def increment(self):
+        self.victims += 1
+
+class Python(Snake):
+    pass
+
+print(Python.__name__, 'es una', Snake.__name__)
+print(Python.__bases__[0].__name__, 'puede ser una', Python.__name__)
+
+
